a0e9932f285189bb2b618009ddc7ced111df5963
Adam <groth.adam.91@gmail.com>
2022-03-01 14:09:47 +0100

    Installerat Styled components

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import Styling from '../components/Footer.module.css'
 
 function Footer() {
   return (
     <div>
       
       <section>
-        <h2>Kontakta oss</h2>
+        <h2 className={Styling.titleText}>Kontakta oss</h2>
         <p>Telefon: 08-0000000</p>
         <p>Mail: kontakt@mail.se</p>
         <p>Södervägen 12 A, Stockholm</p>
